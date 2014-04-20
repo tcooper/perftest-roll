0cd05ebc805fd5708061455dbd72f114c38c66a0
Trevor Cooper <tcooper@sdsc.edu>
2014-04-19 22:50:48 -0700

    Updated fio

--- src/fio/version.mk
+++ src/fio/version.mk
@@ -1,6 +1,14 @@
-PKGROOT		= /opt/fio
-NAME		= fio
-VERSION		= 2.1.2
-RELEASE		= 0
-TARBALL_POSTFIX	= tar.gz
-RPM.EXTRAS      = "AutoReq: no"
+PKGROOT            = /opt/fio
+NAME               = opt-fio
+VERSION            = 2.1.7
+RELEASE            = 0
+
+SRC_SUBDIR         = fio
+
+SOURCE_NAME        = fio
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.bz2
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_BZ2_PKGS       = $(SOURCE_PKG)
