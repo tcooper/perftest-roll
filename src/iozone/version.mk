a873cd5f8febec4d59655e688d6cf9a139a916e7
Trevor Cooper <tcooper@sdsc.edu>
2014-08-08 15:25:03 -0700

    Updated Makefile and version.mk for iozone

--- src/iozone/version.mk
+++ src/iozone/version.mk
@@ -1,7 +1,15 @@
 PKGROOT         = /opt/iozone
-NAME		= iozone
-VERSION		= 3.420
-IOZONE_VERSION  = 3_420
-RELEASE		= 0
-TARBALL_POSTFIX = tar
+NAME            = opt-iozone
+VERSION         = 3_428
+RELEASE         = 0
 RPM.EXTRAS      = "AutoReq: no"
+
+SRC_SUBDIR      = iozone
+
+SOURCE_NAME     = iozone
+SOURCE_VERSION  = $(VERSION)
+SOURCE_SUFFIX   = tar.gz
+SOURCE_PKG      = $(SOURCE_NAME)$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR      = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS     = $(SOURCE_PKG)
