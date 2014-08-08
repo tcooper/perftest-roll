6e70cbaa7394cf1ada7e7e45a1f7734d6b319bac
Trevor Cooper <tcooper@sdsc.edu>
2014-08-08 16:03:57 -0700

    Updated Makefile and version.mk for nfswatch.

--- src/nfswatch/version.mk
+++ src/nfswatch/version.mk
@@ -1,6 +1,15 @@
-PKGROOT		= /opt/nfswatch
-NAME		= nfswatch
+NAME		= opt-nfswatch
 VERSION		= 4.99.11
 RELEASE		= 0
 TARBALL_POSTFIX	= tar.gz
 RPM.EXTRAS      = "AutoReq: no"
+
+SRC_SUBDIR      = nfswatch
+
+SOURCE_NAME     = nfswatch
+SOURCE_VERSION  = $(VERSION)
+SOURCE_SUFFIX   = tar.gz
+SOURCE_PKG      = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR      = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS     = $(SOURCE_PKG)
