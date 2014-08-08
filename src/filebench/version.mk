b14f23d12a100d58e64fe3ca0150fb71de806ff3
Trevor Cooper <tcooper@sdsc.edu>
2014-08-08 15:09:08 -0700

    Updated Makefile and version.mk for filebench

--- src/filebench/version.mk
+++ src/filebench/version.mk
@@ -1,12 +1,12 @@
+NAME               = opt-filebench
 PKGROOT            = /opt/filebench
-NAME               = filebench
 VERSION            = 1.4.9.1
 RELEASE            = 0
 RPM.EXTRAS         = AutoReq:No
 
 SRC_SUBDIR         = filebench
 
-SOURCE_NAME        = $(NAME)
+SOURCE_NAME        = filebench
 SOURCE_VERSION     = $(VERSION)
 SOURCE_SUFFIX      = tar.gz
 SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
