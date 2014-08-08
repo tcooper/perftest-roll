ceac2660b5f48e2070ca288e52c11f4328738d25
Trevor Cooper <tcooper@sdsc.edu>
2014-08-08 16:33:07 -0700

    Updated filebench-modules files

--- src/filebench-modules/version.mk
+++ src/filebench-modules/version.mk
@@ -1,3 +1,9 @@
+# Get the filebench version from it's version.mk file. See Makefile
+VERSION.MK.MASTER = version.mk
+VERSION.MK.MASTER.DIR = ../filebench
+VERSION.MK.INCLUDE = filebench.version.mk
+
+include $(VERSION.MK.INCLUDE) 
 NAME		= filebench-modules
-VERSION		= 6.1
-RELEASE		= 0
+RELEASE		= 1
+RPM.REQUIRES	= environment-modules
