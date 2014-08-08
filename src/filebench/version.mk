504dc4fa32dfa2d6733c111e1e9425e2618eff13
Trevor Cooper <tcooper@sdsc.edu>
2014-08-08 15:01:44 -0700

    Updated Makefile and version.mk

--- src/filebench/version.mk
+++ src/filebench/version.mk
@@ -1,6 +1,14 @@
-PKGROOT		= /opt/filebench
-NAME		= filebench
-VERSION		= 1.4.9.1
-RELEASE 	= 0
-TARBALL_POSTFIX	= tar.gz
-RPM.EXTRAS      = "AutoReq: no"
+PKGROOT            = /opt/filebench
+NAME               = filebench
+VERSION            = 1.4.9.1
+RELEASE            = 0
+RPM.EXTRAS         = AutoReq:No
+
+SRC_SUBDIR         = filebench
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
