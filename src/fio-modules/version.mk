6361e64a0b274e9c29d412f62179f7d78c5dccbe
Trevor Cooper <tcooper@sdsc.edu>
2014-08-08 16:31:20 -0700

    Update fio-modules files

--- src/fio-modules/version.mk
+++ src/fio-modules/version.mk
@@ -1,3 +1,9 @@
+# Get the fio version from it's version.mk file. See Makefile
+VERSION.MK.MASTER = version.mk
+VERSION.MK.MASTER.DIR = ../fio
+VERSION.MK.INCLUDE = fio.version.mk
+
+include $(VERSION.MK.INCLUDE) 
 NAME		= fio-modules
-VERSION		= 6.1
-RELEASE		= 0
+RELEASE		= 1
+RPM.REQUIRES	= environment-modules
