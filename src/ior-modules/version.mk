59344493c6dc919854d1aac5e958d84151332c1a
Trevor Cooper <tcooper@sdsc.edu>
2014-08-13 17:28:01 -0700

    Updated ior-modules

--- src/ior-modules/version.mk
+++ src/ior-modules/version.mk
@@ -1,3 +1,9 @@
+# Get the ior version from it's version.mk file. See Makefile
+VERSION.MK.MASTER = version.mk
+VERSION.MK.MASTER.DIR = ../ior
+VERSION.MK.INCLUDE = ior.version.mk
+
+include $(VERSION.MK.INCLUDE) 
 NAME		= ior-modules
-VERSION		= 6.1
-RELEASE		= 0
+RELEASE		= 1
+RPM.REQUIRES	= environment-modules
