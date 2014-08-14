f4c98dffb3b8f609f52374f0242ac9adb2d99bdc
Trevor Cooper <tcooper@sdsc.edu>
2014-08-13 18:04:14 -0700

    Updated fio version

--- src/fio/version.mk
+++ src/fio/version.mk
@@ -1,13 +1,13 @@
 NAME               = opt-fio
-VERSION            = 2.1.7
+VERSION            = 2.1.11
 RELEASE            = 0
 
 SRC_SUBDIR         = fio
 
 SOURCE_NAME        = fio
 SOURCE_VERSION     = $(VERSION)
-SOURCE_SUFFIX      = tar.bz2
+SOURCE_SUFFIX      = tar.gz
 SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
-TAR_BZ2_PKGS       = $(SOURCE_PKG)
+TAR_GZ_PKGS        = $(SOURCE_PKG)
