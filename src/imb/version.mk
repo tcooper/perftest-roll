eee78eb0f7691ef2233ca3161feacef184d26b10
Trevor Cooper <tcooper@sdsc.edu>
2014-08-26 12:16:26 -0700

    Updated imb build

--- src/imb/version.mk
+++ src/imb/version.mk
@@ -1,14 +1,13 @@
-PKGROOT            = /opt/imb
-NAME               = imb
+NAME               = opt-imb
 VERSION            = 4.0
 RELEASE            = 0
 RPM.EXTRAS         = AutoReq:No
 
 SRC_SUBDIR         = imb
 
-SOURCE_NAME        = $(NAME)
+SOURCE_NAME        = imb
 SOURCE_VERSION     = $(VERSION)
 SOURCE_SUFFIX      = tgz
-SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_PKG         = $(SOURCE_NAME)_$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 
 TGZ_PKGS           = $(SOURCE_PKG)
