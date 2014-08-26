772d816b2449e0ea3c035b292eb3275d67b6deae
Trevor Cooper <tcooper@sdsc.edu>
2014-08-26 12:12:19 -0700

    Updated collectl build

--- src/collectl/version.mk
+++ src/collectl/version.mk
@@ -1,12 +1,11 @@
-PKGROOT            = /opt/collectl
-NAME               = collectl
+NAME               = opt-collectl
 VERSION            = 3.6.7
 RELEASE            = 0
 RPM.EXTRAS         = "AutoReq: no"
 
 SRC_SUBDIR         = collectl
 
-SOURCE_NAME        = $(NAME)
+SOURCE_NAME        = collectl
 SOURCE_VERSION     = $(VERSION)
 SOURCE_SUFFIX      = tar.gz
 SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).src.$(SOURCE_SUFFIX)
