fb9d7c9f43505c26aefd564cf0a29aefd17e4875
Trevor Cooper <tcooper@sdsc.edu>
2014-08-26 12:13:22 -0700

    Updated collect-modules build

--- src/collectl-modules/version.mk
+++ src/collectl-modules/version.mk
@@ -1,3 +1,9 @@
-NAME    	= collectl-modules
-VERSION 	= 6.1
-RELEASE		= 0
+# Get the collectl version from it's version.mk file. See Makefile
+VERSION.MK.MASTER = version.mk
+VERSION.MK.MASTER.DIR = ../collectl
+VERSION.MK.INCLUDE = collectl.version.mk
+
+include $(VERSION.MK.INCLUDE) 
+NAME		= collectl-modules
+RELEASE		= 1
+RPM.REQUIRES	= environment-modules
