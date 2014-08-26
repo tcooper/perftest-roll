34bcf9fae0bc9cac932f3cd4d7f35098c84a21d1
Trevor Cooper <tcooper@sdsc.edu>
2014-08-26 12:17:38 -0700

    Updated imb-modules build

--- src/imb-modules/version.mk
+++ src/imb-modules/version.mk
@@ -1,3 +1,9 @@
+# Get the imb version from it's version.mk file. See Makefile
+VERSION.MK.MASTER = version.mk
+VERSION.MK.MASTER.DIR = ../imb
+VERSION.MK.INCLUDE = imb.version.mk
+
+include $(VERSION.MK.INCLUDE) 
 NAME		= imb-modules
-VERSION		= 6.1.1
-RELEASE		= 0
+RELEASE		= 1
+RPM.REQUIRES	= environment-modules
