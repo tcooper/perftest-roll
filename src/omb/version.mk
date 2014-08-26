198a3054c9f2ad67e2bfe1c033be6de3d1cec220
Trevor Cooper <tcooper@sdsc.edu>
2014-08-26 12:18:39 -0700

    Updated omb build

--- src/omb/version.mk
+++ src/omb/version.mk
@@ -1,14 +1,14 @@
-PKGROOT            = /opt/omb
-NAME               = osu-micro-benchmarks
+NAME               = opt-omb
 VERSION            = 4.3.1
 RELEASE            = 0
 RPM.EXTRAS         = AutoReq:No
 
 SRC_SUBDIR         = omb
 
-SOURCE_NAME        = $(NAME)
+SOURCE_NAME        = osu-micro-benchmarks
 SOURCE_VERSION     = $(VERSION)
 SOURCE_SUFFIX      = tar.gz
 SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_NAME)-$(SOURCE_VERSION)
 
 TAR_GZ_PKGS        = $(SOURCE_PKG)
